8389ea954d7ce450c5a0ee6774dbbb816d1b6b57
Tyler Furgalack <104029862+tylerfurgalack@users.noreply.github.com>
2023-11-13 15:46:40 -0500

    updated model and created userSeeder

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-console */
-import { connection } from "../boot.js"
+import { connection } from "../boot.js";
+import UserSeeder from "./seeders/UserSeeder.js";
 
 class Seeder {
   static async seed() {
     // include individual seed commands here
 
-    console.log("Done!")
-    await connection.destroy()
+    console.log("seeding users...");
+    await UserSeeder.seed();
+
+    console.log("Done!");
+    await connection.destroy();
   }
 }
 
-export default Seeder
+export default Seeder;
