2a385e32e43eafaffa715d89f766fbb5879dba72
Tyler Furgalack <104029862+tylerfurgalack@users.noreply.github.com>
2023-11-15 09:07:50 -0500

    migration for chats table

---
+++ server/src/db/migrations/20231114211500_createMatches.cjs
@@ -0,0 +1,23 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+  return knex.schema.createTable("chats", (table) => {
+    table.bigIncrements("id");
+    table.bigInteger("senderId").unsigned().notNullable().index().references("users.id");
+    table.bigInteger("receiverId").unsigned().notNullable().index().references("users.id");
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists("chats");
+};
